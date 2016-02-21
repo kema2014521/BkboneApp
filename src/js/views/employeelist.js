5f7b6a1727fc773b2ddf11202dbc416453c233df
Ke Ma <kema2014521@gmail.com>
2016-02-21 12:52:07 -0500

    Backbone single page application
    flexible, maintainable, extensible code

---
+++ src/js/views/employeelist.js
@@ -0,0 +1,50 @@
+window.EmployeeListView = Backbone.View.extend({
+
+    tagName:'ul',
+
+    className:'nav nav-list',
+
+    initialize:function () {
+        var self = this;
+        this.model.bind("reset", this.render, this);
+        this.model.bind("add", function (employee) {
+            $(self.el).append(new EmployeeListItemView({model:employee}).render().el);
+        });
+    },
+
+    render:function () {
+        $(this.el).empty();
+        _.each(this.model.models, function (employee) {
+                        
+            $(this.el).append(new EmployeeListItemView({model:employee}).render().el);
+        }, this);
+        return this;
+    }
+});
+
+window.EmployeeListItemView = Backbone.View.extend({
+
+    tagName:"li",
+
+    initialize:function () {
+        this.model.bind("change", this.render, this);
+        this.model.bind("destroy", this.close, this);
+    },
+
+    events: {
+       //"keyup #employee": "updateName"
+    },
+    
+    updateName: function(e) {
+    
+      // assume user finish input
+       if (e.keyCode==13) 
+         this.model.set('firstName', $('.name', this.el).val()); 
+    },
+    
+    render:function () {
+        $(this.el).html(this.template(this.model.toJSON()));
+        return this;
+    }
+
+});
